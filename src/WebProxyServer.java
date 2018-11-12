31519318bcf442aa3eb29c613f75e5c1f28ecba1
Jaeguins <jsu4838@naver.com>
2018-11-13 00:09:05 +0900

    proxy end,artifact add

--- src/WebProxyServer.java
+++ src/WebProxyServer.java
@@ -1,161 +1,39 @@
 
 
-import com.oracle.tools.packager.Log;
-
 import java.io.*;
 import java.net.*;
-import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
-public class WebProxyServer {
-    public static void main(String[] args) {
-        ServerSocketHandler serverHandler = new ServerSocketHandler(80);
-        serverHandler.turnOn();
-    }
-}
-class SocketHandler{
-    Socket socket;
-    String sub="";
-    BufferedReader reader;
-    DataOutputStream writer;
-    public SocketHandler(Socket socket){
-        this.socket=socket;
-        sub+=socket.getInetAddress();
-        LogStation.log(sub,"connected");
-        try {
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            writer = new DataOutputStream(socket.getOutputStream());
-        }catch(IOException e){e.printStackTrace();}
-    }
-    public void react()throws IOException{
-        try {
-            String poppedMsg = readLine();
-            String data = "";
-            while (!poppedMsg.equals("")) {
-                LogStation.log(sub, poppedMsg);
-                data += poppedMsg + "\n";
-                poppedMsg = readLine();
-            }
-            String[] lines = data.split("\n");
-            String[][] headers = new String[lines.length][];
-            headers[0] = lines[0].substring(0, lines[0].lastIndexOf("HTTP/1.0")).split(" ");
-            for (int i = 1; i < lines.length; i++) {
-                headers[i] = lines[i].split(": ");
-            }
-            byte[] out;
-
-        switch(headers[0][1]){
-            case "/":
-                notFound();
-                break;
-            default:
-                URL url = new URL("http://"+headers[0][1].substring(1));
-                Socket socket=new Socket(url.getHost(),80);
-                OutputStream output=socket.getOutputStream();
-
-                output.write(("GET / HTTP/1.1").getBytes());
-                LogStation.log("buffer","GET / HTTP/1.1");
-                output.write(("HOST: "+headers[0][1].substring(1)).getBytes());
-                LogStation.log("buffer","HOST: "+headers[0][1].substring(1));
-                for(int i=2;i<lines.length;i++){
-                    if(lines[i].substring(0,"Cookie".length()).equals("Cookie"))continue;
-                    output.write(lines[i].getBytes());
-                    LogStation.log("buffer",lines[i]);
-                }
-                output.flush();
-                streamData(socket.getInputStream());
-                break;
-        }
-        }catch(IOException e){
-            e.printStackTrace();
-            notFound();
-        }
-    }
-    public String readLine()throws IOException{
-        return reader.readLine();
-    }
-    public void writeData(String input)throws IOException{
-        writeData(input.getBytes());
-    }
-    public void streamData(InputStream stream)throws IOException{
-        BufferedReader rd=new BufferedReader(new InputStreamReader(stream));
-        String buf=rd.readLine();
-        while(!buf.equals("")) {
-            writer.writeBytes(buf);
-            LogStation.log("buffered msg",buf);
-            buf = rd.readLine();
-        }
-        writer.flush();
-    }
-    public void writeData(byte[] data)throws IOException{
-        writer.writeBytes("HTTP/1.1 200 OK \r\n");
-        writer.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-        writer.writeBytes("Content-Length: " + data.length + "\r\n");
-        writer.writeBytes("\r\n");
-        writer.write(data, 0, data.length);
-        writer.writeBytes("\r\n");
-        writer.flush();
-    }
-    public void redirect()throws IOException{
-        writer.writeBytes("HTTP/1.1 301 Moved Permanently\r\n");
-        writer.writeBytes("Location: http://127.0.0.1/html/index.html\r\n");
-        writer.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-        writer.writeBytes("\r\n");
-        writer.writeBytes("\r\n");
-        writer.flush();
-    }
-    public void notFound()throws IOException{
-        byte[] data="404 Not Found".getBytes();
-        writer.writeBytes("HTTP/1.1 404 Not Found\r\n");
-        writer.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-        writer.writeBytes("Content-Length: " + data.length + "\r\n");
-        writer.writeBytes("\r\n");
-        writer.write(data, 0, data.length);
-        writer.writeBytes("\r\n");
-        writer.flush();
-    }
-
-    public void writerClose()throws IOException{
-        writer.close();
-    }
-    public void readerClose()throws IOException{
-        reader.close();
-    }
-    public void close()throws IOException{
-        socket.close();
-    }
-}
-class ServerSocketHandler{
+public class WebProxyServer{
     public ServerSocket serverSocket;
     public boolean serverWaiting=true;
-    Queue<SocketHandler> queue= new LinkedList<>();
-    Thread thread;
-
+    Queue<Reactor> queue= new LinkedList<>();
     public boolean isEmpty(){
         return queue.isEmpty();
     }
-    public SocketHandler pop(){
+    public Reactor pop(){
         return queue.poll();
     }
-    public ServerSocketHandler(int port){
+    public WebProxyServer(int port){
         try {
             serverSocket=new ServerSocket(port);
+            turnOn();
         } catch (IOException e) {e.printStackTrace();}
     }
     public void turnOn(){
         serverWaiting=true;
         while(serverWaiting){
             try {
-                SocketHandler handler=new SocketHandler(serverSocket.accept());
+                LogStation.log("server","listening ready.");
+                Reactor handler=new Reactor(serverSocket.accept());
                 queue.offer(handler);
             }catch(IOException e){e.printStackTrace();}
             if(!isEmpty()){
-                SocketHandler popped=pop();
-                new Thread(() -> {
-                    try{
-                        popped.react();
-                    }catch(IOException e){e.printStackTrace();}
-                }).start();
+                Reactor popped=pop();
+                new Thread(popped).start();
             }
         }
     }
@@ -167,10 +45,115 @@ class ServerSocketHandler{
             e.printStackTrace();
         }
     }
-}
-
-class LogStation{//logger
-    public static void log(String sub,String msg){
-        System.out.println(sub+" : "+msg);
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("$Input port : ");
+        WebProxyServer w=new WebProxyServer(scan.nextInt());
     }
 }
+class Reactor implements Runnable{
+    String sub="";
+    Socket sock;
+    public Reactor(Socket socket){
+        this.sock=socket;
+        sub+=socket.getInetAddress();
+        LogStation.log(sub,"connected");
+    }
+    @Override
+    public void run() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            String poppedMsg = reader.readLine();
+            String data = "";
+            String slaveData = "";
+            byte[] slaveBuf;
+            while (poppedMsg!=null&&!poppedMsg.equals("")) {
+                data += poppedMsg + "\n";
+                poppedMsg = reader.readLine();
+            }
+            HTTPReceiver parser = new HTTPReceiver(data);
+            String localPath=("./caches/"+parser.target.substring(7).replace('?','/').replace('\\','/')+".cache");
+            File cache=new File(localPath);
+            Path pathToFile=Paths.get(localPath);
+            Files.createDirectories(pathToFile.getParent());
+            if(!cache.isFile()){
+                String host=new URL(parser.target).getHost();
+                URL url=new URL(parser.target);
+                URLConnection urlConn=url.openConnection();
+                urlConn.setDoOutput(true);
+                urlConn.setDoInput(true);
+                urlConn.setUseCaches(true);
+                List<String> keyList = new ArrayList<>(parser.field.keySet());
+                List<String> valueList = new ArrayList<>(parser.field.values());
+                for(int i=0;i<keyList.size();i++){
+                    urlConn.setRequestProperty(keyList.get(i),valueList.get(i));
+                }
+                InputStream in=urlConn.getInputStream();
+                slaveBuf=in.readAllBytes();
+                LogStation.log(host, parser.target);
+                FileOutputStream fio=new FileOutputStream(localPath);
+                fio.write(slaveBuf);
+                fio.close();
+            }else {
+                slaveBuf=new FileInputStream(cache).readAllBytes();
+                LogStation.log("file", localPath);
+            }
+            OutputStream masterOut=sock.getOutputStream();
+
+            masterOut.write(slaveBuf);
+            LogStation.log("proxyOut",localPath);
+            reader.close();
+            masterOut.flush();
+            masterOut.close();
+            sock.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+class LogStation{//logger
+    public static void log(String sub,String msg){
+        System.out.println("$ "+sub+" :"+msg);
+    }
+}
+
+class HTTPReceiver{
+    String info;
+    String target;
+    String version;
+    public LinkedHashMap<String,String> field=new LinkedHashMap<>();
+    public HTTPReceiver(String input){
+        String[] lines=input.split("\n");
+        String[] header=lines[0].split(" ");
+        info=header[0];
+        target=header[1];
+        version=header[2];
+        for(int i=1;i<lines.length;i++) {
+            String[] tmp = lines[i].split(": ");
+            field.put(tmp[0], tmp[1]);
+        }
+    }
+    public String toPacket(){
+        StringBuilder stringBuilder=new StringBuilder();
+        String space=" ";
+        String line="\r\n";
+        String diff=": ";
+        stringBuilder.append(info);
+        stringBuilder.append(space);
+        stringBuilder.append(target.substring(target.indexOf('/',8)));
+        stringBuilder.append(space);
+        stringBuilder.append(version);
+        stringBuilder.append(line);
+        List<String> keyList = new ArrayList<>(field.keySet());
+        List<String> valueList = new ArrayList<>(field.values());
+        for(int i=0;i<keyList.size();i++){
+            stringBuilder.append(keyList.get(i));
+            stringBuilder.append(diff);
+            stringBuilder.append(valueList.get(i));
+            stringBuilder.append(line);
+        }
+        return stringBuilder.toString();
+    }
+}
+
